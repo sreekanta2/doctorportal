39445e6bc2ae9ac0ed36904dd725ca1dfe7a9568
sreekanta2 <sreekanta03s@gmail.com>
2025-08-28 03:02:31 -0600

    done

--- app/(pages)/clinics/[clinicId]/loading.tsx
+++ app/(pages)/clinics/[clinicId]/loading.tsx
@@ -2,7 +2,7 @@ import Loader from "@/components/loader";
 
 export default function loading() {
   return (
-    <div>
+    <div className="h-screen flex items-center justify-center">
       <Loader />
     </div>
   );
